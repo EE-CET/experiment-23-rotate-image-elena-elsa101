588ec10bbc5e3295468a7fbf24c7387b708a1d77
elena-elsa101 <elenaelsacherian@gmail.com>
2026-03-28 23:46:51 +0530

    Update RotateImage.java

--- src/RotateImage.java
+++ src/RotateImage.java
@@ -1,9 +1,37 @@
+  import java.util.*;
 public class RotateImage {
-    
-        // TODO: Read n and n (dimensions)
-        // TODO: Read the matrix elements
-        // TODO: Rotate the matrix 90 degrees clockwise INPLACE
-        // Hint: 1. Transpose the matrix. 2. Reverse each row.
-        // TODO: Print the rotated matrix
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+sc.nextInt();
+int[][] a=new int[n][n];
+for(int i=0;i<n;i++)
+for(int j=0;j<n;j++)
+a[i][j]=sc.nextInt();
+
+for(int i=0;i<n;i++)
+for(int j=i+1;j<n;j++){
+int t=a[i][j];
+a[i][j]=a[j][i];
+a[j][i]=t;
+}
+
+for(int i=0;i<n;i++){
+for(int j=0;j<n/2;j++){
+int t=a[i][j];
+a[i][j]=a[i][n-1-j];
+a[i][n-1-j]=t;
+}
+}
+
+for(int i=0;i<n;i++){
+for(int j=0;j<n;j++){
+System.out.print(a[i][j]);
+if(j<n-1) System.out.print(" ");
+}
+if(i<n-1) System.out.println();
+}
+}
+
     
 }
